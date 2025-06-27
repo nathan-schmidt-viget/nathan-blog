9163b107515985f08ddfddb3fc63dfce398db26e
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-06-27 09:54:00 -0600

    [n/a] Updating content

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,43 @@ import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
-    <section>
-      <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
-        Nathan Schmidt
-      </h1>
-      <p className='mb-4'>
-        {`Always building. Nathan started out as a construction worker before deciding that he’d rather build websites than houses. He earned degrees in web development and graphic design and has now been creating digital products for more than a decade. A Colorado native, Nathan loves applying creative thinking to web development and bringing interactive components to life.`}
-      </p>
+    <section className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-2'>
+        <p className='text-sm text-neutral-600 dark:text-neutral-400'>
+          Senior UI Developer
+        </p>
+        <h1 className='text-5xl font-semibold tracking-tighter'>
+          Nathan Schmidt
+        </h1>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <p>
+          I'm a software engineer based in Colorado. I'm currently working at{" "}
+          <a
+            href='https://www.viget.com'
+            target='_blank'
+            className='underline hover:no-underline focus-visible:no-underline'
+            rel='noopener noreferrer'
+          >
+            Viget
+          </a>
+          .
+        </p>
+        <p>
+          I build UI components for sites and multi million dollar web
+          applications in collaboration with other developers and or
+          independently. I take high fidelity designs and developing them into
+          large design systems that can be used throughout the application.
+        </p>
+        <p>
+          I develop interactive UI components that meet accessibility standards
+          WCAG. I collaborate with stakeholders to gather feedback, refine
+          requirements, and align on project goals. I ensure that the UI
+          components effectively meet the needs of both users and business
+          objectives. I'm a quick learner and I'm always looking for new
+          challenges.
+        </p>
+      </div>
       <div className='my-8'>
         <BlogPosts />
       </div>
