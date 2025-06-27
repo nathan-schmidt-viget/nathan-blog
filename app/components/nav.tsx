234d674b16c10f2218253c2704d0c08809b0d264
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-06-27 08:34:53 -0600

    [n/a] Updating the content width

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,7 +14,7 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className='-ml-[8px] mb-16 tracking-tight'>
+    <header className='-ml-[8px] mb-16 tracking-tight'>
       <div className='lg:sticky lg:top-20'>
         <nav
           className='flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative'
@@ -35,6 +35,6 @@ export function Navbar() {
           </div>
         </nav>
       </div>
-    </aside>
+    </header>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -44,16 +44,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang='en'
-      className={cx(
-        "text-black bg-white dark:text-white dark:bg-black",
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <body className='antialiased max-w-xl mx-4 mt-8 lg:mx-auto'>
-        <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
+    <html lang='en' className={cx("", GeistSans.variable, GeistMono.variable)}>
+      <body className='text-black bg-white dark:text-white dark:bg-black'>
+        <main className='bg-blur-2xl relative z-10 antialiased max-w-2xl mx-4 lg:mx-auto flex-auto mt-6 flex flex-col p-2'>
           <Navbar />
           {children}
           <Footer />
